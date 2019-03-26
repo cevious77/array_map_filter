19bd96aa3061f86fb0951496461807c0a13e9704
Marot <48023487+cevious77@users.noreply.github.com>
2019-03-26 17:02:29 +0100

    Add files via upload

---
+++ map/ex5.js
@@ -0,0 +1,33 @@
+/* Array.prototype.map - Exercice 5
+
+Ecrire une fonction multiplyOddIndices, qui prend en entrée un tableau de nombres.
+* Chaque nombre situé à un index pair doit être renvoyé tel quel
+* Chaque nombre situé à un index impair doit être renvoyé multiplié par son index
+
+Il va donc falloir utiliser le 2ème argument de la fonction passée à map.
+
+Exemple de tableau d'entrée:
+  [3, 8, 11, 13, 19, 7]
+Tableau renvoyé par multiplyOddIndices:
+  [3, 8, 11, 39, 19, 35]
+*/
+
+function multiplyOddIndices(numbers) {
+
+return numbers.map( number => {
+
+  if (numbers.indexOf(number) % 2 === 0) {
+    return number
+    // nbre pair
+} else {
+  return (numbers.indexOf(number) * number)
+    // nbre impair
+}
+
+
+})
+
+}
+
+
+module.exports = multiplyOddIndices;
